877dbb86b2692606b21e8c1c47cd6e3a78af7f61
Zach <zkdoran@gmail.com>
2023-07-01 17:02:39 -0400

    users initial setup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: {
+        user: {
+          username: @user.username,
+        }
+      }
+    else
+      render json: {
+        success: false
+      }
+    end
+  end
 end
