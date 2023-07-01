f275688018cd7537749889f26128e0925839718e
Zach <zkdoran@gmail.com>
2023-07-01 18:24:33 -0400

    tokens not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
       render json: {
         user: {
           username: @user.username,
+          email: @user.email
         }
       }
     else
@@ -14,4 +15,10 @@ class UsersController < ApplicationController
       }
     end
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,4 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 64 }
   validates :email, presence: true, length: { minimum: 5, maximum: 500 }
   validates :password, presence: true, length: { minimum: 8, maximum: 64 }
-
-  after_validation :hash_password
-
-  private
-
-  def hash_password
-    self.password = BCrypt::Password.create(self.password)
-  end
 end
